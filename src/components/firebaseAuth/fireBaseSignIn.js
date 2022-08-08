2307cc41c32c5c01de25406ee9ede226ee46e96e
RohitShah1706 <rlshah03@gmail.com>
2022-08-08 19:41:05 +0530

    added signinWithRedirect to firebase sign in

--- src/components/firebaseAuth/fireBaseSignIn.js
+++ src/components/firebaseAuth/fireBaseSignIn.js
@@ -1,15 +1,27 @@
 import { app } from './firebaseConfig';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { isDesktop } from 'react-device-detect';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithRedirect } from "firebase/auth";
 const fireBaseSignIn = () => {
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
-    return signInWithPopup(auth, provider)
-        .then(result => {
-            return result.user;
-        })
-        .catch(err => {
-            console.log(err);
-            return null
-        })
+    if (isDesktop) {
+        return signInWithPopup(auth, provider)
+            .then(result => {
+                return result.user;
+            })
+            .catch(err => {
+                console.log(err);
+                return null
+            })
+    }
+    else {
+        return signInWithRedirect(auth, provider)
+            .then(result => {
+                return result.user;
+            }).catch(err => {
+                console.log(err);
+                return null
+            })
+    }
 }
 export { fireBaseSignIn };
